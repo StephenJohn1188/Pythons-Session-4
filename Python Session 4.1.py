2e1c444df256a14a8ce8e61448ba296c16d6ec15
Santhosh Kumar Mahalingam <99348330+StephenJohn1188@users.noreply.github.com>
2022-03-26 15:56:30 +0530

    Add files via upload

---
+++ Python Session 4.1.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Sets
+
+# In[1]:
+
+
+x={12,15,87,97,56,98}
+x
+
+
+# In[4]:
+
+
+days={'Sunday','Tuesday','Wednesday','Thursday'}
+
+
+# In[5]:
+
+
+days
+
+
+# In[6]:
+
+
+days.add('Monday')
+
+
+# In[7]:
+
+
+days
+
+
+# In[8]:
+
+
+days.pop()
+
+
+# In[9]:
+
+
+days
+
+
+# In[11]:
+
+
+days.clear()
+
+
+# In[12]:
+
+
+days
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
